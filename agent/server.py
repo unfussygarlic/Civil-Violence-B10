71996107a987a99a02c520e60cecb8a9bf9abf15
Savoyevatel <71535298+Savoyevatel@users.noreply.github.com>
2020-10-04 11:15:35 -0400

    Update server.py

--- agent/server.py
+++ agent/server.py
@@ -4,14 +4,20 @@ from .params import model_params, gridsize
 from mesa.visualization.modules import CanvasGrid, ChartModule, TextElement
 from mesa.visualization.ModularVisualization import ModularServer
 from .portrayal import agent_portrayal
-
+"""
+   
+   
+"""
 
 class Core_parameters(TextElement):
     def render(self, model):
         corruption = model.corruption
         democracy = model.democracy
         employment = model.employment
-
+"""      
+        
+        Displaying current count of Rich class people, Middle class people, Poor class people
+"""
         text = f"Corruption: {round(corruption*100,2)}%  Democracy: {round(democracy*100,2)}%  Employment: {round(employment*100,2)}% <br> \
                 Rich count: {model.r_c}({model.r_a_c}) Middle count: {model.m_c}({model.m_a_c}) Poor count: {model.p_c}({model.p_a_c})"
 
