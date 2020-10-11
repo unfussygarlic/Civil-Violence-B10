688805f60d6448d4a78fc6b79404acdeda9e9102
Savoyevatel <71535298+Savoyevatel@users.noreply.github.com>
2020-10-11 13:14:48 -0400

    Update people.py

--- agent/people.py
+++ agent/people.py
@@ -1,26 +1,14 @@
 import numpy as np
 from mesa import Agent
 from .authority import Cop
-from .params import k_c, k_d, k_e, k_p, k_af, r_c
-from .params import wealth_inc, timestep, confidence_threshold
-from .params import jail_period
-
-# An agent that emulates the behavior of a citizen, he can be a rich
-# class citizen, middle class citizen or poor citizen.
-# A citizen can go 3 states: Calm, Revolt, Jail
-# It has the following attributes:
-#     unique_id
-#     model
-#     agent_type = cop or citizen
-#     wealth
-
+from .params import k_c, k_d, k_e, k_p, k_af
+from .params import wealth_inc, timestep, confidence_threshold, kill_threshold
+from .params import jail_period, angry_threshold, dead_threshold
 
 class Citizen(Agent):
     def __init__(self, unique_id, model, agent_type):
         super().__init__(unique_id, model)
         self.alignment = "Citizen"
-
-        # Randomizing each class of the citizen
 
         cat = ["Rich", "Middle", "Poor"]
         self.status = np.random.choice(cat, p=[0.33, 0.33, 0.34])
@@ -40,12 +28,12 @@ class Citizen(Agent):
         self.j_time = 0
         self.n_j = 0
 
-    # Decide whether the move is applicable
     def step(self):
         self.update_jail_time()
         self.update_wealth()
         self.update_neighbors()
         self.measure_confidence()
+        self.kill_cops()
         if self.movement:
             self.update_state()
             self.move()
@@ -61,10 +49,8 @@ class Citizen(Agent):
         else:
             pass
 
-    # Updates each iteration to see if his state will change
-
     def update_state(self):
-        if self.confidence > confidence_threshold and self.random.random() > r_c:
+        if self.confidence > confidence_threshold:
             self.state = "Revolt"
         else:
             self.state = "Calm"
@@ -72,8 +58,6 @@ class Citizen(Agent):
     def update_wealth(self):
         net_wealth = wealth_inc[self.status] * (1 - self.model.corruption + 0.1)
         self.wealth += net_wealth
-
-    # Look around and see for neighbors
 
     def update_neighbors(self):
         neighborhood = self.model.grid.get_neighborhood(self.pos, moore=True, radius=1)
@@ -87,12 +71,6 @@ class Citizen(Agent):
                     self.citizens.append(neighbor)
                 elif neighbor.alignment == "Cop":
                     self.cops.append(neighbor)
-
-    def kill_cops(self):
-        # TODO
-        pass
-
-    # movement of the agent
 
     def move(self):
         if self.empty_cells:
@@ -114,6 +92,16 @@ class Citizen(Agent):
         self.risk_factor = k_af * np.exp(-(n_c / n_g))
 
         if self.cops:
+            self.confidence = 1-(self.grievance * self.risk_factor)
+        elif self.citizens:
             self.confidence = self.grievance * self.risk_factor
-        else:
-            self.confidence = self.grievance
+
+    def kill_cops(self):
+            for i in self.cops:
+                if (i.n_cop > dead_threshold) and (len(self.citizens)> 6) and (self.model.negotiation > self.confidence):
+                    self.model.kill_agents.append(i)
+                else:
+                    pass 
+    
+
+
