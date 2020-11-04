d11a43403a7508d64de4ee5fc792a08ab6480008
Savoyevatel <71535298+Savoyevatel@users.noreply.github.com>
2020-11-04 18:26:43 -0400

    Update gradient.py

--- utils/gradient.py
+++ utils/gradient.py
@@ -1,6 +1,6 @@
-"""
-Code by KindaSorta : https://stackoverflow.com/questions/59132840/python-continuous-colour-grading
-"""
+#Code by KindaSorta : https://stackoverflow.com/questions/59132840/python-continuous-colour-grading
+#showing the grievance among agents, only for displaying purposes
+
 def hex_to_RGB(hex):
     """ "#FFFFFF" -> [255,255,255] """
     # Pass 16 to the integer function for change of base
